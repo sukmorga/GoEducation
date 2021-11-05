b868fa4b3556275583140fb8009e59492a6df6f6
Konstantin <naginks@outlook.com>
2021-11-05 19:34:53 +0200

    lesson3

--- main.go
+++ main.go
@@ -1,31 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	var a int = 27
-	fmt.Println(a)
+	// num := 3
+	// if num > 0 {
+	// 	fmt.Println("Number is greater then 0")
+	// } else if num < 0 {
+	// 	fmt.Println("Number < 0")
+	// } else if num == 3 {
+	// 	fmt.Println("Number equals 3!!!")
+	// }
 
- 	var number float64 = 27.45454
-	fmt.Println(number)
+	var a float64
+	var b float64
+	var c float64
 
-	name := "Денис"
-	fmt.Println(name)
-	fmt.Println(len(name))
+	fmt.Println("Реши квадратное уравнение")
+	fmt.Println("Введи а:")
+	fmt.Scan("&a")
 
-	var name2 string
-	var age uint
-	fmt.Println("Whats is your name?")
-	fmt.Scan(&name2)
-	fmt.Println("Hello " + name2 + "!")
-	fmt.Println("How old are you?")
-	fmt.Scan(&age)
-	fmt.Println("You are " + fmt.Sprint(age) + " years")
+	fmt.Println("Введи b:")
+	fmt.Scan("&b")
 
-	var h int8 = 2
-	var g int64 = int64(h)
-	fmt.Println(g)
+	fmt.Println("Введи с:")
+	fmt.Scan("&c")
 
-	//********************************************************LESSON2
+	d := (b * b) - 4 * (a * c)
+	
+	if d > 0 {
+		var x1 float64
+		var x2 float64
+
+		x1 = (-b + math.Sqrt(d)) / (2 * a)
+		x2 = (-b - math.Sqrt(d)) / (2 * a)
+
+		fmt.Println("Ваше уравнение имеет 2 корня \n d = " + fmt.Sprint(d))
+		fmt.Println("X1: " + fmt.Sprint(x1) + "\nX2: " + fmt.Sprint(x2))
+	} else if d == 0 {
+		var x float64
+
+		x = (-b / (2 * a))
+
+		fmt.Println("Ваше уравнение имеет один корень")
+		fmt.Println("X: " + fmt.Sprint(x))
+	} else {
+		fmt.Println("Ваше уравнение не имеет корней \n d = " + fmt.Sprint(d))
+	}
+
+	//********************************************************LESSON3
 
 }
