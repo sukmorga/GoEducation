d0861a626ee3029c2043744e762e09936d1b1001
Konstantin <naginks@outlook.com>
2021-11-05 18:57:55 +0200

    step2

--- main.go
+++ main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var a int = 27
+	fmt.Println(a)
+
+ 	var number float64 = 27.45454
+	fmt.Println(number)
+
+	name := "Денис"
+	fmt.Println(name)
+	fmt.Println(len(name))
+
+	var name2 string
+	var age uint
+	fmt.Println("Whats is your name?")
+	fmt.Scan(&name2)
+	fmt.Println("Hello " + name2 + "!")
+	fmt.Println("How old are you?")
+	fmt.Scan(&age)
+	fmt.Println("You are " + fmt.Sprint(age) + " years")
+
+	var h int8 = 2
+	var g int64 = int64(h)
+	fmt.Println(g)
+
+	//********************************************************LESSON2
+
 }
